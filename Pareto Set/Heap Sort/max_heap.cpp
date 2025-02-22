7729376e6273ccf34504fbcc39ec1b1d2fd3a361
Hardi <hardivirani2003@gmail.com>
2025-02-21 19:16:19 -0500

    Building Heap

---
+++ Pareto Set/Heap Sort/max_heap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Build heap -> TC O(n)
+// Heapify -> TC O(logn)
+
+void build_heap(int arr[], int n)
+{
+    for (int i = n / 2; i >= 0i --)
+    {
+        maxheapify(arr, n, i);
+    }
+}
+
+int main()
+{
+}
