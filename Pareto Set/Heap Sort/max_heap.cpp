21e72cc7532171ee7fe9fc41be3eaed9b73b883d
Hardi <hardivirani2003@gmail.com>
2025-02-21 19:20:30 -0500

    maxheapify

--- Pareto Set/Heap Sort/max_heap.cpp
+++ Pareto Set/Heap Sort/max_heap.cpp
@@ -4,6 +4,28 @@ using namespace std;
 // Build heap -> TC O(n)
 // Heapify -> TC O(logn)
 
+void maxheapify(int arr[], int n)
+{
+    int largest = i;
+    int left = (2 * i) + 1;
+    int right = (2 * i) + 2;
+
+    if (left < n && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        maxheapify(arr, n, largest);
+    }
+}
+
 void build_heap(int arr[], int n)
 {
     for (int i = n / 2; i >= 0i --)
